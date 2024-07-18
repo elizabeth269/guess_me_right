dfb9a77c831bd75b1b2a8fd2c3e0db7609024592
Elizbeth <elizabethadebayo269@gmail.com>
2024-07-18 22:52:26 +0100

    added some juice

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,16 @@ use std::{cmp::Ordering, io};
 fn main() {
     //introduce the game
     println!("Welcome to the guessing game for people between age 18 and 25.");
+    println!("What is your name?");
 
-    //get user input
     let mut name = String::new();
-    let mut age = String::new();
-
     io::stdin()
         .read_line(&mut name)
         .expect("Your name should have a number.");
+    println!("what is your age?");
+    //get user input
+    let mut age = String::new();
+
     io::stdin()
         .read_line(&mut age)
         .expect("couldn't read user input");
@@ -18,13 +20,15 @@ fn main() {
     let age: u32 = age.trim().parse().expect("your age should be a number");
 
     //restrict certain users from playing the game.
-    if 18 > age && age < 25 {
-        println!("Welcome{name}");
+    if 18 <= age && age <= 25 {
+        println!("Welcome {name}");
         //generate secret number
+        let mut counter = 0;
+        let secret_number = rand::thread_rng().gen_range(1..=100);
+        println!("secret number: {secret_number}");
+        println!("Guess a number between 1 and 100");
         loop {
-            let secret_number = rand::thread_rng().gen_range(1..=100);
-            println!("secret number: {secret_number}");
-
+            counter += 1;
             let mut guess = String::new();
             io::stdin()
                 .read_line(&mut guess)
@@ -42,7 +46,8 @@ fn main() {
                 }
             }
         }
+        println!("Dear {name} You tried {counter} times, weldone.");
     } else {
-        println!("Dear {name}, you are not allowed to play the game. Only people between 18 and 25 are allowed.")
+        println!("Dear {name} you are not allowed to play the game. Only people between 18 and 25 are allowed.")
     }
 }
