f07b9c73b301aeadc2b90fcdacd3c9dfe73ad167
Elizbeth <elizabethadebayo269@gmail.com>
2024-07-18 22:14:53 +0100

    get user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    //introduce the game
+    println!("Welcome to the guessing game for people between age 18 and 25.");
+
+    //get user input
+    let mut name = String::new();
+    let mut age = String::new();
+
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Your name should have a number.");
+    io::stdin()
+        .read_line(&mut age)
+        .expect("couldn't read user input");
+
+    let age: u32 = age.trim().parse().expect("your age should be a number");
 }
