747d5e0e27f2d2ce5f49d649a5b7ed2620ca8cf8
Elizbeth <elizabethadebayo269@gmail.com>
2024-07-18 22:37:48 +0100

    wrote the full code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use rand::Rng;
-use std::io;
+use std::{cmp::Ordering, io};
 fn main() {
     //introduce the game
     println!("Welcome to the guessing game for people between age 18 and 25.");
@@ -20,6 +20,28 @@ fn main() {
     //restrict certain users from playing the game.
     if 18 > age && age < 25 {
         println!("Welcome{name}");
+        //generate secret number
+        loop {
+            let secret_number = rand::thread_rng().gen_range(1..=100);
+            println!("secret number: {secret_number}");
+
+            let mut guess = String::new();
+            io::stdin()
+                .read_line(&mut guess)
+                .expect("couldn't read the message");
+            let guess: u32 = match guess.trim().parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+            };
+            match guess.cmp(&secret_number) {
+                Ordering::Less => println!("too small"),
+                Ordering::Greater => println!("too big"),
+                Ordering::Equal => {
+                    println!("you win");
+                    break;
+                }
+            }
+        }
     } else {
         println!("Dear {name}, you are not allowed to play the game. Only people between 18 and 25 are allowed.")
     }
